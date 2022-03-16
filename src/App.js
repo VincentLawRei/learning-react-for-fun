80b8f781f3456a2e4de486680187c730168fc782
Vincent Law <sekainome@protonmail.com>
2022-03-16 05:53:27 +0400

    Before useFetching: adding loader component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './styles/App.css'
 import {PostList} from './components/PostList';
 import {PostForm} from './components/PostForm';
@@ -6,6 +6,8 @@ import {PostFilter} from './components/PostFilter';
 import MyModal from "./components/UI/MyModal/MyModal";
 import {MyButton} from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -14,7 +16,14 @@ function App() {
         {id: 3, title: 'Cavascript 3', body: 'Eescription'},
     ])
 
+    const [modal, setModal] = useState(false)
     const [filter, setFilter] = useState({sort: '', query: ''})
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [isPostsLoading, setIsPostsLoading] = useState(false)
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -25,24 +34,33 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
-    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-
-    const [modal, setModal] = useState(false)
-
+    async function fetchPosts() {
+        setIsPostsLoading(true)
+        setTimeout(async () => {
+            const posts = await PostService.getAll()
+            setPosts(posts)
+            setIsPostsLoading(false)
+        }, 1000)
+    }
 
     return (
         <div className="App">
+            <button onClick={fetchPosts}>GET POSTS</button>
             <MyModal visible={modal}
                      setVisible={setModal}>
                 <PostForm create={createPost}/>
             </MyModal>
             <PostFilter filter={filter}
                         setFilter={setFilter}/>
-            <PostList remove={removePost}
-                      posts={sortedAndSearchedPosts}
-                      title={'Список постов'}/>
+            {isPostsLoading
+                ? <div style={{display: "flex", justifyContent: "center", marginTop: "50px"}}><Loader/></div>
+                : <PostList remove={removePost}
+                            posts={sortedAndSearchedPosts}
+                            title={'Список постов'}/>
+            }
 
-            <MyButton style={{marginTop: "15px"}} onClick={() => setModal(true)}>
+            <MyButton style={{marginTop: "15px"}}
+                      onClick={() => setModal(true)}>
                 Написать пост
             </MyButton>
 
